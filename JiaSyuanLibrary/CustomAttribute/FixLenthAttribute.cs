69a9e91308354fe851a7efd15b9cddeaf1952318
JiaSyuan Li <jiasyuan@hotmail.com>
2019-11-21 20:05:30 +0800

    Add FixLenthAttribute

---
+++ JiaSyuanLibrary/CustomAttribute/FixLenthAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JiaSyuanLibrary.CustomAttribute
+{
+    /// <summary>
+    /// Fix Lenth Attribute
+    /// </summary>
+    public class FixLenthAttribute: Attribute
+    {
+        /// <summary>
+        /// Fix Lenth Order
+        /// </summary>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Length
+        /// </summary>
+        public int Length{ get; set; }
+
+        /// <summary>
+        /// Repeat Times
+        /// </summary>
+        public int RepeatTimes { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsPadLeft { get; set; } = true;
+
+    }
+}
