208c2617b93ebe35d4ab3992d669baeebb2d8675
JiaSyuan Li <jiasyuan@hotmail.com>
2019-11-21 20:16:56 +0800

    Add  FixLenthHelper

---
+++ JiaSyuanLibrary/Helper/FixLenthHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiaSyuanLibrary.Helper
+{
+    /// <summary>
+    /// FixLenthHelper
+    /// </summary>
+    public static class FixLenthHelper
+    {
+        /// <summary>
+        /// Fix Lenth String Deserialize To Object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="FixLenthString"></param>
+        /// <returns></returns>
+        public static T FixLenthDeserialize<T>(string FixLenthString)
+        {
+            CheckLenth(FixLenthString);
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Object Serialize To Fix Lenth String
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="InputModel"></param>
+        /// <returns></returns>
+        public static string FixLenthSerialize<T>(T InputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FixLenthString"></param>
+        /// <returns></returns>
+        private static bool CheckLenth(string FixLenthString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
