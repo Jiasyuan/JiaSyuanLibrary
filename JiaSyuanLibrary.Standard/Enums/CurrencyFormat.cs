4c9cea003aabce4402ae90d1fa286cb7cdf0dcdf
JiaSyuan Li <jiasyuan@hotmail.com>
2019-11-28 21:24:57 +0800

    Create JiaSyuanLibrary.Standard

---
+++ JiaSyuanLibrary.Standard/Enums/CurrencyFormat.cs
@@ -0,0 +1,15 @@
+﻿namespace JiaSyuanLibrary.Standard.Enums
+{
+    public enum CurrencyFormat
+    {
+        /// <summary>
+        /// 中文大寫
+        /// </summary>
+        Chinese,
+        /// <summary>
+        /// 整數Comma
+        /// </summary>
+        Comma,
+        CommaReservedDecimalNumber
+    }
+}
