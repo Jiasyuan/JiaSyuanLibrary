bb25ba9d4c12c55cc2e6e17a36f57046398a96b9
JiaSyuan Li <jiasyuan@hotmail.com>
2019-12-07 21:38:37 +0800

    Add StringExtensions

---
+++ JiaSyuanLibrary.Standard/Extension/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace JiaSyuanLibrary.Standard.Extension
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Convert String To Int Nullable
+        /// </summary>
+        /// <param name="inPut"></param>
+        /// <returns></returns>
+        public static int? StringToIntNullable(this string inPut)
+        {
+            if (int.TryParse(inPut, out int result))
+                return result;
+            return null;
+        }
+
+        /// <summary>
+        /// Convert String To decimal Nullable
+        /// </summary>
+        /// <param name="inPut"></param>
+        /// <param name="decimalPlaces"></param>
+        /// <returns></returns>
+        public static decimal? StringToDecimalNullable(this string inPut, int decimalPlaces)
+        {
+            if (string.IsNullOrWhiteSpace(inPut))
+            {
+                return null;
+            }
+            else
+            {
+                inPut = inPut.Insert(inPut.Length - decimalPlaces, ".");
+                if (decimal.TryParse(inPut, out decimal result))
+                    return result;
+                return null;
+            }
+        }
+    }
+}
