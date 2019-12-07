c08038f66ea1912123453f6c8431ddbe20018706
JiaSyuan Li <jiasyuan@hotmail.com>
2019-12-07 22:00:48 +0800

    Implement FixLength Deserialize

--- JiaSyuanLibrary.Standard/CustomAttribute/FixLengthAttribute.cs
+++ JiaSyuanLibrary.Standard/CustomAttribute/FixLengthAttribute.cs
@@ -5,7 +5,7 @@ namespace JiaSyuanLibrary.Standard.CustomAttribute
     /// <summary>
     /// Fix Length Attribute
     /// </summary>
-    public class FixLengthAttribute: Attribute
+    public class FixLengthAttribute : Attribute
     {
         /// <summary>
         /// Total Length
@@ -15,22 +15,36 @@ namespace JiaSyuanLibrary.Standard.CustomAttribute
         /// <summary>
         /// Fix Length Order
         /// </summary>
-        public int Order { get; set; }
+        public int Order { get; set; } = 0;
 
         /// <summary>
         /// Length
         /// </summary>
-        public int Length{ get; set; }
+        public int Length { get; set; }
 
         /// <summary>
         /// Repeat Times
         /// </summary>
-        public int RepeatTimes { get; set; }
+        public int RepeatTimes { get; set; } = 0;
 
         /// <summary>
         /// Is Pad Left
         /// </summary>
         public bool IsPadLeft { get; set; } = true;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public char PadChar { get; set; } = ' ';
+
+        /// <summary>
+        /// Decimal places
+        /// </summary>
+        public int DecimalPlaces { get; set; }
+        /// <summary>
+        /// DateTime Format
+        /// </summary>
+        public string DateTimeFormat { get; set; } = "yyyy/MM/dd HH:mm:ss";
 
     }
 }
--- JiaSyuanLibrary.Standard/Helper/FixLengthHelper.cs
+++ JiaSyuanLibrary.Standard/Helper/FixLengthHelper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using JiaSyuanLibrary.Standard.CustomAttribute;
+using JiaSyuanLibrary.Standard.Extension;
 
 namespace JiaSyuanLibrary.Standard.Helper
 {
@@ -21,12 +23,25 @@ namespace JiaSyuanLibrary.Standard.Helper
         /// <returns></returns>
         public static T FixLengthDeserialize<T>(string fixLengthString)
         {
-            TypeInfo typeInfo = typeof(T).GetTypeInfo();
-            var modelCustomAttribute = typeInfo.GetCustomAttributes().FirstOrDefault() as FixLengthAttribute;
-            int totalLength = modelCustomAttribute?.TotalLength ?? 0;
-            if (CheckLength(fixLengthString, totalLength))
+            Type type = typeof(T);
+            var modelCustomAttribute = type.GetTypeInfo().GetCustomAttributes().FirstOrDefault() as FixLengthAttribute;
+            if (CheckLength(fixLengthString, modelCustomAttribute?.TotalLength ?? 0))
             {
-                //TODO:FixLenthDeserialize
+                var topPropertyInfos = GetPropertyInfosAndSort(type);
+                int index = 0;
+                T firstModel = Activator.CreateInstance<T>();
+                foreach (var topPropertyInfo in topPropertyInfos)
+                {
+                    if (typeof(IEnumerable).IsAssignableFrom(topPropertyInfo.PropertyType) &&
+                        topPropertyInfo.PropertyType != typeof(string))
+                    {
+                        Console.WriteLine(topPropertyInfo.Name);
+                    }
+                    else
+                    {
+                        SetPropertyValue<T>(topPropertyInfo, fixLengthString, ref index, ref firstModel);
+                    }
+                }
             }
             else
             {
@@ -47,13 +62,102 @@ namespace JiaSyuanLibrary.Standard.Helper
         }
 
         /// <summary>
-        /// 
+        /// Check Length
         /// </summary>
         /// <param name="fixLengthString"></param>
+        /// <param name="totalLength"></param>
         /// <returns></returns>
         private static bool CheckLength(string fixLengthString, int totalLength)
         {
-            throw new NotImplementedException();
+            return LengthCount(fixLengthString) == totalLength;
+        }
+
+        /// <summary>
+        /// String Length Count
+        /// </summary>
+        /// <param name="fixLengthString"></param>
+        /// <returns></returns>
+        private static int LengthCount(string fixLengthString)
+        {
+            int result = 0;
+            if (!string.IsNullOrEmpty(fixLengthString))
+            {
+                result = fixLengthString.Length;
+                result += fixLengthString.ToCharArray().Count(t => Convert.ToInt32(t) > 255);
+                return result;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Set Property Value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyInfo"></param>
+        /// <param name="fixLengthString"></param>
+        /// <param name="index"></param>
+        /// <param name="model"></param>
+        private static void SetPropertyValue<T>(PropertyInfo propertyInfo, string fixLengthString, ref int index, ref T model)
+        {
+            if (!(propertyInfo.GetCustomAttributes().FirstOrDefault() is FixLengthAttribute attribute))
+            {
+                throw new Exception($"Property:{propertyInfo.Name} without FixLengthAttribute");
+            }
+            else
+            {
+                model.GetType().GetProperty(propertyInfo.Name)?.SetValue(model, StringToPropertyType(fixLengthString, propertyInfo.PropertyType, attribute, ref index));
+            }
+        }
+
+        /// <summary>
+        /// String To Property Type
+        /// </summary>
+        /// <param name="fixLengthString"></param>
+        /// <param name="propertyType"></param>
+        /// <param name="attribute">Property's FixLengthAttribute</param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static dynamic StringToPropertyType(string fixLengthString, Type propertyType, FixLengthAttribute attribute, ref int index)
+        {
+            string propertyValueString = fixLengthString.Substring(index, attribute.Length);
+            index += attribute.Length;
+            if (propertyType == typeof(string))
+            {
+                int propertyValueStringLength = LengthCount(propertyValueString);
+                if (propertyValueStringLength > attribute.Length)//全形處理
+                {
+                    int differenceIndex = propertyValueStringLength - attribute.Length;
+                    propertyValueString = propertyValueString.Substring(0, differenceIndex);
+                    index -= differenceIndex;
+                }
+                return propertyValueString.Trim();
+            }
+            else if (propertyType == typeof(decimal?))
+            {
+                return fixLengthString.StringToDecimalNullable(attribute.DecimalPlaces);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get PropertyInfos And Sort Properties
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static IEnumerable<PropertyInfo> GetPropertyInfosAndSort(Type type)
+        {
+            Type fixLengthAttributeType = typeof(FixLengthAttribute);
+            return type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Select(x => new
+            {
+                Property = x,
+                Attribute = (FixLengthAttribute)Attribute.GetCustomAttribute(x, fixLengthAttributeType, true)
+            })
+                .OrderBy(x => x.Attribute?.Order ?? -1)
+                .Select(x => x.Property);
         }
     }
 }
