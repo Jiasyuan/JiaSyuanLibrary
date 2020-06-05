13c030546889ad9f8041dc5dd0e26c3085e05b59
JiaSyuan Li <jiasyuan@hotmail.com>
2020-06-05 14:18:52 +0800

    Add GZipDecompressionAttribute & GZipCompressionAttribute

---
+++ JiaSyuanLibrary/CustomAttribute/_Filter/GZipCompressionAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Web.Http.Filters;
+using System.IO;
+using Ionic.Zlib;
+
+namespace JiaSyuanLibrary.CustomAttribute
+{
+
+    public class GZipCompressionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actContext)
+        {
+            var content = actContext.Response.Content;
+            var bytes = content?.ReadAsByteArrayAsync().Result;
+            var zipContent = bytes == null ? new byte[0] : Compress(bytes);
+            actContext.Response.Content = new ByteArrayContent(zipContent);
+            actContext.Response.Content.Headers.Remove("Content-Type");
+            actContext.Response.Content.Headers.Add("Content-encoding", "gzip");
+
+            //actContext.Response.Content.Headers.Add("Content-Type",     "application/json");
+            actContext.Response.Content.Headers.Add("Content-Type", "application/json;charset=utf-8");
+            base.OnActionExecuted(actContext);
+        }
+
+        private static byte[] Compress(byte[] sourceBytes)
+        {
+            if (sourceBytes == null)
+            {
+                return null;
+            }
+
+            using (var outputStream = new MemoryStream())
+            {
+                using (var zipStream = new GZipStream(outputStream,
+                    CompressionMode.Compress,
+                    CompressionLevel.BestSpeed,
+                    true))
+                {
+                    zipStream.Write(sourceBytes, 0, sourceBytes.Length);
+                }
+
+                return outputStream.ToArray();
+            }
+        }
+    }
+}
---
+++ JiaSyuanLibrary/CustomAttribute/_Filter/GZipDecompressionAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Ionic.Zlib;
+
+namespace JiaSyuanLibrary.CustomAttribute
+{
+    public class GZipDecompressionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var content = actionContext.Request.Content;
+            var zipContentBytes = content?.ReadAsByteArrayAsync().Result;
+            var unzipContentBytes = zipContentBytes == null ? new byte[0] : Decompress(zipContentBytes);
+            actionContext.Request.Content = new ByteArrayContent(unzipContentBytes);
+            base.OnActionExecuting(actionContext);
+        }
+
+        private static byte[] Decompress(byte[] sourceBytes)
+        {
+            using (var compressedStream = new MemoryStream(sourceBytes))
+            using (var zipStream = new GZipStream(compressedStream,
+                CompressionMode.Decompress,
+                CompressionLevel.BestSpeed,
+                true))
+
+            using (var outputStream = new MemoryStream())
+            {
+                zipStream.CopyTo(outputStream);
+                return outputStream.ToArray();
+            }
+        }
+    }
+}
