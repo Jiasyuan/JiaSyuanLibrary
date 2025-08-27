618cde331c86fe6b9a6fe7740285bbb2c8434b91
JiaSyuan <jiasyuan@hotmail.com>
2025-08-28 01:20:12 +0800

    Create FixLengthFormatter

---
+++ Src/DotNetFramework/JiaSyuanLibrary.NetFramework/Helper/FixLengthFormatter/Attribute/FixLengthAttribute.cs
@@ -0,0 +1,44 @@
+﻿namespace JiaSyuanLibrary.NetFramework.Helper.FixLengthFormatter.Attribute
+{
+    public class FixLengthAttribute : System.Attribute
+    {
+        ///// <summary>
+        ///// Total Length
+        ///// </summary>
+        //public int TotalLength { get; set; }
+
+        /// <summary>
+        /// Fix Length Order
+        /// </summary>
+        public int Order { get; set; } = 0;
+
+        /// <summary>
+        /// Length
+        /// </summary>
+        public int Length { get; set; }
+
+        ///// <summary>
+        ///// Repeat Times
+        ///// </summary>
+        //public int RepeatTimes { get; set; } = 0;
+
+        /// <summary>
+        /// Is Pad Left
+        /// </summary>
+        public bool IsPadLeft { get; set; } = true;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public char PadChar { get; set; } = ' ';
+
+        /// <summary>
+        /// Decimal places
+        /// </summary>
+        public int DecimalPlaces { get; set; }
+        /// <summary>
+        /// DateTime Format
+        /// </summary>
+        public string DateTimeFormat { get; set; } = "yyyy/MM/dd HH:mm:ss";
+    }
+}
